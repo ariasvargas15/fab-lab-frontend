2bf96f61b1be447fde1b76d0a72507b0863040fe
bsarias <brayan.arias@condorlabs.io>
2022-06-28 23:30:50 -0500

    Remove unused classes

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -1,24 +1,10 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
-import {AlertsComponent} from './alerts/alerts.component';
 import {FeatherModule} from 'angular-feather';
 import {allIcons} from 'angular-feather/icons';
 import {ProyectoComponent} from './proyecto/proyecto.component';
 import {DemoFlexyModule} from '../demo-flexy-module';
-import {GridListComponent} from './grid-list/grid-list.component';
-import {MenuComponent} from './menu/menu.component';
-import {TabsComponent} from './tabs/tabs.component';
-import {ExpansionComponent} from './expansion/expansion.component';
-import {ChipsComponent} from './chips/chips.component';
-import {ProgressComponent} from './progress/progress.component';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-import {ToolbarComponent} from './toolbar/toolbar.component';
-import {ProgressSnipperComponent} from './progress-snipper/progress-snipper.component';
-import {SnackbarComponent} from './snackbar/snackbar.component';
-import {SliderComponent} from './slider/slider.component';
-import {SlideToggleComponent} from './slide-toggle/slide-toggle.component';
-import {ButtonsComponent} from './buttons/buttons.component';
-import {TooltipsComponent} from './tooltips/tooltips.component'
 import {LoginComponent} from "./login/login.component";
 import {MatFormFieldModule} from "@angular/material/form-field";
 import {MatInputModule} from "@angular/material/input";
@@ -28,21 +14,7 @@ import {MatIconModule} from "@angular/material/icon";
 
 @NgModule({
     declarations: [
-        AlertsComponent,
         ProyectoComponent,
-        GridListComponent,
-        MenuComponent,
-        TabsComponent,
-        ExpansionComponent,
-        ChipsComponent,
-        ProgressComponent,
-        ToolbarComponent,
-        ProgressSnipperComponent,
-        SnackbarComponent,
-        SliderComponent,
-        SlideToggleComponent,
-        ButtonsComponent,
-        TooltipsComponent,
         LoginComponent
     ],
     imports: [
@@ -57,20 +29,7 @@ import {MatIconModule} from "@angular/material/icon";
         MatButtonModule,
     ],
     exports: [
-        AlertsComponent,
         ProyectoComponent,
-        GridListComponent,
-        MenuComponent,
-        TabsComponent,
-        ExpansionComponent,
-        ChipsComponent,
-        ProgressComponent,
-        ToolbarComponent,
-        ProgressSnipperComponent,
-        SnackbarComponent,
-        SliderComponent,
-        SlideToggleComponent,
-        ButtonsComponent,
         LoginComponent
     ]
 })
